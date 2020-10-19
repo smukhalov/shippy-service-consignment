00b412118d0ae1d9a02e1f540296c73a52d914ce
Sergey Mukhalov <smukhalov@gmail.com>
2020-10-19 20:13:36 +0300

    3

--- main.go
+++ main.go
@@ -66,10 +66,10 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	return &pb.Response{Created: true, Consignment: consignment}, nil
 }
 
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
-	consignments := s.repo.GetAll()
-	return &pb.Response{Consignments: consignments}, nil
-}
+// func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+// 	consignments := s.repo.GetAll()
+// 	return &pb.Response{Consignments: consignments}, nil
+// }
 
 func main() {
 
